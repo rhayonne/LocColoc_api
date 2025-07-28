e612514f5517f84587bb0d817e7fac75869726a7
Rhayonne Lopes <rhayonne@gmail.com>
2025-07-28 16:38:50 +0200

    Creation module imb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ImbPtsModule } from './imb_pts/imb_pts.module';
 import { ImbModule } from './imb/imb.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ImbModule],
+  imports: [UserModule, ImbModule, ImbPtsModule],
   controllers: [AppController],
   providers: [AppService],
 })
